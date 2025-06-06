21e296db9da8e0979d1989320f6f42eee3ebbea6
llydn <dnn0226@gmail.com>
2025-06-06 01:08:46 -0700

    change keybindings in nvim-tree

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -9,12 +9,14 @@ local function my_on_attach(bufnr)
     api.config.mappings.default_on_attach(bufnr)
 
     -- custom mappings
-    -- vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent,        opts("Up"))
-    -- vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
     vim.keymap.set("n", "s", api.node.open.vertical, opts("Open: Vertical splib"))
     vim.keymap.set("n", "i", api.node.open.horizontal, opts("Open: Horizontal Split"))
 
     vim.keymap.set("n", "<C-x>", api.node.run.system, opts("Run System"))
+    vim.keymap.del("n", "<C-v>", { buffer = bufnr })  -- remove default mapping for <C-v>
+
+    vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
+    vim.keymap.del("n", "<C-t>", { buffer = bufnr })  -- remove default mapping for <C-t>
 end
 
 -- return my_on_attach
