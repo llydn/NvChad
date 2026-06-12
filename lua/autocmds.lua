efd1d3a6a2e949f230699cda0c7ed576cd504985
llydn <dnn0226@gmail.com>
2026-06-12 14:43:20 -0700

    enable yank highlights

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
